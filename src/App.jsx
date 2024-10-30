1c98abd946ec13bcd55f3597d63e234cb80c9bf3
Pratishtha Verma <pratishthaverma2000@gmail.com>
2024-10-31 03:56:38 +0530

    page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import ContactForm from "./components/ContactForm";
-// import SheetDataViewer from "./components/SheetDataViewer";
-// import SheetPage from "./pages/SheetPage";
-// import { Routes, Route } from "react-router-dom";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div>
       <ContactForm />
-    </>
+    </div>
   );
 }
 
